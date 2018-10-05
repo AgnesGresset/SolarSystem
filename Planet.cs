fb5b1964f970b2eb01b1b1e24a5885b3241011aa
Agnes Gresset <agnes.gresset@hotmail.fr>
2018-10-05 10:58:44 +0200

    Draft of base classes Planet Satellite and Interface

---
+++ Planet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonnensystem
+{
+    class Planet : SolarSystemInterface
+    {
+        private string Name { get; set; }
+        Satellite satellite = new Satellite();
+
+        public Planet()
+        {
+        }
+
+        public Planet(string name)
+        {
+            Name = name;
+        }
+
+        public Planet(string name, Satellite satellite)
+        {
+            Name = name;
+            this.satellite = satellite;
+        }
+
+        public void sortPlanets()
+        {
+            // GetAllPlanets and sort them here.
+        }
+    }
+}
---
+++ Satellite.cs
@@ -0,0 +1,15 @@
+﻿namespace Sonnensystem
+{
+    class Satellite
+    {
+        private string Name { get; set; }
+        public Satellite()
+        {
+        }
+
+        public Satellite(string name)
+        {
+            Name = name;
+        }
+    }
+}
