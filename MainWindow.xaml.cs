59656caceb1c960b14ca9efff912e9292fe8a016
Aniesse <agnes.gresset@hotmail.fr>
2018-11-05 21:43:30 +0100

    tried to use data from sqlite in combobox but missing main method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,16 +20,27 @@ namespace SolarSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        private dataAccess database = new dataAccess();
+
         public MainWindow()
         {
             InitializeComponent();
+            fill_combo();
         }
 
-        /*
+        private void fill_combo()
+        {
+            List<Planet> planets = database.GetAllPlanets();
+
+            foreach (Planet planet in planets)
+            {
+                planetListingComboBox.Items.Add(planet);
+            }
+        }
+
         private void planetListingComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            planetListing.Text = planetListingComboBox.SelectedItem.ToString();
+           // planetListing.Text = database.GetIndividualPlanet().ToString();
         }
-        */
     }
 }
